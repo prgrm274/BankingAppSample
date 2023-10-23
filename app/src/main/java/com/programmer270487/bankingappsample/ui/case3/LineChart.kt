12977622f1b937cd1d8174da8f9719f26d23fb09
prog <email@gmail.com>
2023-10-24 06:34:53 +0700

    Line chart

--- app/src/main/java/com/programmer270487/bankingappsample/ui/case3/LineChart.kt
+++ app/src/main/java/com/programmer270487/bankingappsample/ui/case3/LineChart.kt
@@ -5,20 +5,15 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asComposePaint
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.text.style.TextAlign
-import com.programmer270487.bankingappsample.data.local.case3.LineChartData
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.asComposePaint
 import androidx.compose.ui.unit.dp
-//import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.unit.sp
+import com.programmer270487.bankingappsample.data.local.case3.LineChartData
 
 /*@Composable
 fun LineChart(lineChartData: LineChartData) {
